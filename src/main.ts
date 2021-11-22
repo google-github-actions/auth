4367cbc68ab7c0c1bebf06a598e882fc48385326
Seth Vargo <seth@sethvargo.com>
2021-11-22 11:23:25 -0500

    Output more information in error about missing secrets

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 'use strict';
 
-import * as core from '@actions/core';
+import {
+  exportVariable,
+  getBooleanInput,
+  getIDToken,
+  getInput,
+  setFailed,
+  setOutput,
+  setSecret,
+} from '@actions/core';
 import { WorkloadIdentityClient } from './client/workload_identity_client';
 import { CredentialsJSONClient } from './client/credentials_json_client';
 import { AuthClient } from './client/auth_client';
 import { BaseClient } from './base';
 import { explodeStrings } from './utils';
+
+const secretsWarning =
+  'If you are specifying input values via GitHub secrets, ensure the secret ' +
+  'is being injected into the environment. By default, secrets are not passed ' +
+  'to workflows triggered from forks, including Dependabot.';
 
 /**
  * Executes the main action, documented inline.
@@ -13,15 +26,15 @@ import { explodeStrings } from './utils';
 async function run(): Promise<void> {
   try {
     // Load configuration.
-    const projectID = core.getInput('project_id');
-    const workloadIdentityProvider = core.getInput('workload_identity_provider');
-    const serviceAccount = core.getInput('service_account');
+    const projectID = getInput('project_id');
+    const workloadIdentityProvider = getInput('workload_identity_provider');
+    const serviceAccount = getInput('service_account');
     const audience =
-      core.getInput('audience') || `https://iam.googleapis.com/${workloadIdentityProvider}`;
-    const credentialsJSON = core.getInput('credentials_json');
-    const createCredentialsFile = core.getBooleanInput('create_credentials_file');
-    const tokenFormat = core.getInput('token_format');
-    const delegates = explodeStrings(core.getInput('delegates'));
+      getInput('audience') || `https://iam.googleapis.com/${workloadIdentityProvider}`;
+    const credentialsJSON = getInput('credentials_json');
+    const createCredentialsFile = getBooleanInput('create_credentials_file');
+    const tokenFormat = getInput('token_format');
+    const delegates = explodeStrings(getInput('delegates'));
 
     // Ensure exactly one of workload_identity_provider and credentials_json was
     // provided.
@@ -31,7 +44,8 @@ async function run(): Promise<void> {
     ) {
       throw new Error(
         'The GitHub Action workflow must specify exactly one of ' +
-          '"workload_identity_provider" or "credentials_json"!',
+          '"workload_identity_provider" or "credentials_json"! ' +
+          secretsWarning,
       );
     }
 
@@ -39,14 +53,15 @@ async function run(): Promise<void> {
     if (workloadIdentityProvider && !serviceAccount) {
       throw new Error(
         'The GitHub Action workflow must specify a "service_account" to ' +
-          'impersonate when using "workload_identity_provider"!',
+          'impersonate when using "workload_identity_provider"! ' +
+          secretsWarning,
       );
     }
 
     // Instantiate the correct client based on the provided input parameters.
     let client: AuthClient;
     if (workloadIdentityProvider) {
-      const token = await core.getIDToken(audience);
+      const token = await getIDToken(audience);
       client = new WorkloadIdentityClient({
         projectID: projectID,
         providerID: workloadIdentityProvider,
@@ -72,19 +87,19 @@ async function run(): Promise<void> {
       }
 
       const credentialsPath = await client.createCredentialsFile(runnerTempDir);
-      core.setOutput('credentials_file_path', credentialsPath);
-      core.exportVariable('CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE', credentialsPath);
-      core.exportVariable('GOOGLE_APPLICATION_CREDENTIALS', credentialsPath);
+      setOutput('credentials_file_path', credentialsPath);
+      exportVariable('CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE', credentialsPath);
+      exportVariable('GOOGLE_APPLICATION_CREDENTIALS', credentialsPath);
     }
 
     // Set the project ID environment variables to the computed values.
     const computedProjectID = await client.getProjectID();
-    core.setOutput('project_id', computedProjectID);
-    core.exportVariable('CLOUDSDK_PROJECT', computedProjectID);
-    core.exportVariable('CLOUDSDK_CORE_PROJECT', computedProjectID);
-    core.exportVariable('GCP_PROJECT', computedProjectID);
-    core.exportVariable('GCLOUD_PROJECT', computedProjectID);
-    core.exportVariable('GOOGLE_CLOUD_PROJECT', computedProjectID);
+    setOutput('project_id', computedProjectID);
+    exportVariable('CLOUDSDK_PROJECT', computedProjectID);
+    exportVariable('CLOUDSDK_CORE_PROJECT', computedProjectID);
+    exportVariable('GCP_PROJECT', computedProjectID);
+    exportVariable('GCLOUD_PROJECT', computedProjectID);
+    exportVariable('GOOGLE_CLOUD_PROJECT', computedProjectID);
 
     switch (tokenFormat) {
       case '': {
@@ -94,8 +109,8 @@ async function run(): Promise<void> {
         break;
       }
       case 'access_token': {
-        const accessTokenLifetime = core.getInput('access_token_lifetime');
-        const accessTokenScopes = explodeStrings(core.getInput('access_token_scopes'));
+        const accessTokenLifetime = getInput('access_token_lifetime');
+        const accessTokenScopes = explodeStrings(getInput('access_token_scopes'));
         const serviceAccount = await client.getServiceAccount();
 
         const authToken = await client.getAuthToken();
@@ -106,14 +121,14 @@ async function run(): Promise<void> {
           lifetime: accessTokenLifetime,
         });
 
-        core.setSecret(accessToken);
-        core.setOutput('access_token', accessToken);
-        core.setOutput('access_token_expiration', expiration);
+        setSecret(accessToken);
+        setOutput('access_token', accessToken);
+        setOutput('access_token_expiration', expiration);
         break;
       }
       case 'id_token': {
-        const idTokenAudience = core.getInput('id_token_audience', { required: true });
-        const idTokenIncludeEmail = core.getBooleanInput('id_token_include_email');
+        const idTokenAudience = getInput('id_token_audience', { required: true });
+        const idTokenIncludeEmail = getBooleanInput('id_token_include_email');
         const serviceAccount = await client.getServiceAccount();
 
         const authToken = await client.getAuthToken();
@@ -123,8 +138,8 @@ async function run(): Promise<void> {
           delegates,
           includeEmail: idTokenIncludeEmail,
         });
-        core.setSecret(token);
-        core.setOutput('id_token', token);
+        setSecret(token);
+        setOutput('id_token', token);
         break;
       }
       default: {
@@ -132,7 +147,7 @@ async function run(): Promise<void> {
       }
     }
   } catch (err) {
-    core.setFailed(`Action failed with error: ${err}`);
+    setFailed(`Action failed with error: ${err}`);
   }
 }
 

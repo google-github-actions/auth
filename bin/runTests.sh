041b0078f03fbc39379e3fa9aa8de3b73c64af02
Seth Vargo <seth@sethvargo.com>
2025-07-17 16:29:55 -0400

    Add linters

--- bin/runTests.sh
+++ bin/runTests.sh
@@ -16,4 +16,6 @@ set -eEuo pipefail
 FILES="$(node -e "process.stdout.write(require('node:fs').readdirSync('./', { recursive: true }).filter((e) => {return e.endsWith('.test.ts') && !e.startsWith('node_modules');}).sort().join(' '));")"
 
 set -x
+
+# shellcheck disable=SC2086
 exec node --require ts-node/register --test-reporter spec --test ${FILES}
